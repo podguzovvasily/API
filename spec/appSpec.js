3b67fa824316da08b50c3f91faef291e17ed2a9d
podguzovvasily <podguzovvasily@gmail.com>
2017-10-23 00:10:29 +0300

    Update appSpec.js

--- spec/appSpec.js
+++ spec/appSpec.js
@@ -1,4 +1,5 @@
-const request = require('supertest');
+const should = require('should'),
+supertest = require('supertest');
 const express = require('express');
 const bodyParser = require('body-parser');
 db = require('../server/config/db.js');
@@ -9,11 +10,39 @@ router(app, db);
 describe('GET /users', function() {
   it('respond with json', function() {
     return request(app)
-      .get('/users')
+      .get('/users/1')
       .set('Accept', 'application/json')
       .expect(200)
       .then(response => {
-          assert(response.body.personid, '1')
+          (response.body.personid, '1')
       })
   });
 });
+
+describe('GET /users', function() {
+  it ('should return valid user data for user 1',
+  function (done) {
+
+    supertest(app)
+    .get('/users/2')
+    .expect(200)
+    .end(function (err, res) {
+      res.status.should.equal(200);
+      done();
+    });
+
+  });
+
+    it('should return an error for an invalid user', 
+    function (done) {
+
+      supertest(app)
+      .get('/users/12555555')
+      .expect(404)
+      .end(function (err, res) {
+        res.status.should.equal(404);
+      })
+      done();
+    });
+    
+  });
