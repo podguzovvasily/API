e46bef17f1bbbe081dd7a5a98674edf9ae5689bb
podguzovvasily <podguzovvasily@gmail.com>
2017-10-24 00:39:54 +0300

    Update appSpec.js

--- spec/appSpec.js
+++ spec/appSpec.js
@@ -8,23 +8,10 @@ router = require('../server/router/index');
 const app = express();
 router(app, db);
 
-/*describe('GET /users', function() {
-  fit('respond with json', 
-  function (done) {
-    supertest(app)
-      .get('/users/1')
-      .set('Accept', 'application/json')
-      .expect(200)
-      .then(response => {
-        assert(response.body.personid, '2')
-        done();
-      })
-  });
-});*/
 
 describe('GET /users', function() {
 
-  it('checking that data was created', function() {
+  it('checking that initial data was created', function() {
     return supertest(app)
       .get('/users/1')
       .set('Accept', 'application/json')
@@ -36,70 +23,30 @@ describe('GET /users', function() {
   });
 });
 
-it('checking that data was created', function() {
-  return supertest(app)
-    .get('/users/1546')
-    .set('Accept', 'application/json')
-    .expect(200)
-    .then(response => {
-        expect(response.body.toBeNull).toBeNull()
+
+  it('checking that user is not exist', function() {
+    return supertest(app)
+      .get('/users/1546454')
+      .set('Accept', 'application/json')
+      .expect(200)
+      .then(response => {
+          expect(response.body).toBeNull();
     })
 });
 
-  it('return 204 if user was deleted or not exist', function() {
+  fit('checking that value must match the regular expression', function() {
+    return supertest(app)
+      .get('/users/2')
+      .set('Accept', 'application/json')
+      .expect(200)
+      .then(response => {
+        expect(response.body.firstName).toMatch('Ivan')
+  })
+});
+
+  it('return 204 if user was deleted', function() {
     return supertest(app)
       .delete('/users/4')
       .set('Accept', 'application/json')
       .expect(204) 
-});
-
-  fit('return 200 on put', function() {
-    return supertest(app)
-      .post('/users/')
-      .set('Accept', 'application/json')
-      .expect(200)
-      .then(request => {
-        {
-          response.body.firstName = 'John'
-        }
-      })
-   
-});
-
-// it('return 204 if user was deleted', function() {
-//   return supertest(app)
-//     .patch('/users/4')
-//     .set('Accept', 'application/json')
-//     .expect(204)
-   
-// });
-
-
-
-
-
-// describe('GET /users by id', function() {
-//   // it ('should return valid user data for user 1',
-//   // function (done) {
-
-//   //   supertest(app)
-//   //   .get('/users/2')
-//   //   .expect(200)
-//   //   .end(function (err, res) {
-//   //     res.status.should.equal(200);
-//   //     done();
-//   //   });
-
-//   // });
-
-//     fit('should return an error for an invalid user', function () {
-//       return supertest(app)
-//       .get('/users/12555555')
-//       .expect(404)
-//       .end(function (err, res) {
-//         //res.status.equal(404);
-//       })
-      
-//     });
-    
-//   });
+}); 
