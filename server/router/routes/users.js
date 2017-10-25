b1416aa64dd491b3a1d2ee4188fae7cc4e70aca4
podguzovvasily <podguzovvasily@gmail.com>
2017-10-25 16:26:07 +0300

    Update users.js

--- server/router/routes/users.js
+++ server/router/routes/users.js
@@ -1,5 +1,7 @@
+var Sequelize = require('sequelize');
+const Op = Sequelize.Op;
 module.exports = (app, db) => {
-
+  
   //GET all users
   app.get('/users', function (req, res) {
     db.User.findAll().then((User) => {
@@ -12,24 +14,38 @@ module.exports = (app, db) => {
     db.User.findOne({ where: { id: req.params.id } }).then((User) => {
       if (!User) {
         res.status(404).send(); 
-        return         
+        return;         
       }     
       return res.json(User);
     })
-    
-  });
-
-    //GET single user by firstName
-    app.get('/users/name/:firstName', function (req, res) {
-      db.User.findAll({ where: { firstName: req.params.firstName } }).then((User) => {
-        res.json(User);
-      });
+    .catch((err) => {
+      res.status(500).json({
+        message:err.message
     });
+  });
+});
 
+
+  //GET single user by firstName
+  app.get('/name/:firstName', function (req, res) {
+    db.User.findAll({ where: { firstName: { [Op.like]: '%' + req.params.firstName + '%'} }}).then((User) => {
+      if (res.body == null) {
+        res.status(404).send(); 
+        return;
+      }  
+      return res.status(200).json(User);
+      })
+      .catch((err) => {
+        res.status(500).json({
+          message:err.message
+    });
+  });
+});
 
   // POST single user
   app.post('/users/', function post(req, res) {
     db.User.create({
+      id: req.body.id,
       personid: req.body.personid,
       firstName: req.body.firstName,
       birthday: req.body.birthday,
@@ -39,8 +55,12 @@ module.exports = (app, db) => {
       phone: req.body.phone,
       lastName: req.body.lastName
     }).then((data) => {
-      res.json(data);
-      // res.status(201).send();
+      res.status(201).json(data);
+    })
+    .catch((err) => {
+      res.status(500).json({
+        message:err.message
+  });
     });
   });
 
@@ -73,6 +93,11 @@ module.exports = (app, db) => {
     db.User.destroy({ where: { id: userId } }).then(() => {
       console.log(`User with id ${userId} has been deleted or not exist`);
       res.status(204).send();
+    })
+    .catch((err) => {
+      res.status(500).json({
+        message:err.message
+  });
     });
   });
 };
